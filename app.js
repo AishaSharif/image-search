b6366dea69c615518b7083f172c1fd64c847f0e8
aisha sharif <sharif_aisha@yahoo.com>
2019-07-10 18:52:45 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const API_URL = 'https://api.unsplash.com/search/photos?client_id=11f2ff5a50fcce4df43aa4c897d132d3f5ad4a84ed0aec7be67718deb5120192';
+const loadingImage = document.querySelector('#loadingImage');
+const imageSection = document.querySelector('.images');
+
+loadingImage.style.display = 'none';
+
+form.addEventListener('submit', formSubmitted);
+
+function formSubmitted(event){
+  
+  event.preventDefault();
+
+  const searchTerm = input.value;
+  
+  search(searchTerm)
+    .then(displayImages);
+}
+ 
+function search(searchTerm) {
+  const url = `${API_URL}&query=${searchTerm}`;  
+  imageSection.innerHTML = '';
+  loadingImage.style.display = '';
+
+  return fetch(url)
+    .then(response => response.json())
+    .then(result => {
+      return result.results;
+    });
+}
+
+function displayImages(images) {
+  images.forEach(image => {
+    console.log(image.urls.regular);
+    const imageElement = document.createElement('img');
+    imageElement.src = image.urls.regular;
+    imageSection.appendChild(imageElement);
+  });
+  loadingImage.style.display = 'none';
+}
